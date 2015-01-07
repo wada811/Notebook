44101ab1065b1bb3b7983ce6a340b7273e7a5ffb
wada811 <at.wada811@gmail.com>
2015-01-07 23:35:59 +0900

    Add ButterKnife and inject R.layout.view_calendar_day

--- app/src/main/java/com/wada811/notebook/CalendarAdapter.java
+++ app/src/main/java/com/wada811/notebook/CalendarAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import java.util.Collections;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class CalendarAdapter extends BindableAdapter<LocalDate>{
 
@@ -19,17 +21,6 @@ public class CalendarAdapter extends BindableAdapter<LocalDate>{
         super(context, Collections.<LocalDate>emptyList());
         today = LocalDate.now();
         middle = Integer.MAX_VALUE / 2 + today.getDayOfWeek();
-    }
-
-    @Override
-    public View newView(LayoutInflater inflater, int position, ViewGroup container){
-        return new TextView(getContext());
-    }
-
-    @Override
-    public void bindView(LocalDate item, int position, View view){
-        TextView textView = (TextView)view;
-        textView.setText(item.toString());
     }
 
     @Override
@@ -47,4 +38,40 @@ public class CalendarAdapter extends BindableAdapter<LocalDate>{
         return Days.daysBetween(today, item).getDays();
     }
 
+
+    @Override
+    public View newView(LayoutInflater inflater, int position, ViewGroup container){
+        View view = inflater.inflate(R.layout.view_calendar_day, container, false);
+        ViewHolder holder = new ViewHolder(view);
+        view.setTag(holder);
+        return view;
+    }
+
+    @Override
+    public void bindView(LocalDate item, int position, View view){
+        ViewHolder holder = (ViewHolder)view.getTag();
+        holder.calendarDayDate.setText(item.toString());
+    }
+
+    class ViewHolder{
+
+        @InjectView(R.id.calendarDayTitle)
+        LinearLayout calendarDayTitle;
+        @InjectView(R.id.calendarDayBody)
+        LinearLayout calendarDayBody;
+        @InjectView(R.id.calendarDayDate)
+        TextView calendarDayDate;
+        @InjectView(R.id.calendarDayOverflowPlan)
+        TextView calendarDayOverflowPlan;
+        @InjectView(R.id.calendarDayPlan1)
+        TextView calendarDayPlan1;
+        @InjectView(R.id.calendarDayPlan2)
+        TextView calendarDayPlan2;
+        @InjectView(R.id.calendarDayPlan3)
+        TextView calendarDayPlan3;
+
+        public ViewHolder(View view){
+            ButterKnife.inject(this, view);
+        }
+    }
 }
